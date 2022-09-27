cda4e34906030228b89b4d1e4e90127f6cf4c034
DHIRAJ7472 <dhirajdhande112@gmail.com>
2022-09-27 15:18:51 +0530

    product class is created

---
+++ HelloWorld/Products.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Products
+    {
+        public int Id {  get; set; }
+
+        public string Name { get; set; }
+
+        public int Rate { get; set; }
+
+    }
+}
